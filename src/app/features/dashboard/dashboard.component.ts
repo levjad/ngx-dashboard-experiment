25ff887adc587de974621e74289083107e32e3f9
levjad <levjad@gmail.com>
2019-01-29 10:07:36 +0100

    Changed dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,37 +1,42 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { map, takeUntil } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import { ApiService } from '../../shared/services/api.service';
 import { Todos } from '../../shared/models/todos';
-import { Observable, Subject } from 'rxjs';
+import { Jokes } from '../../shared/models/jokes';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'jad-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent implements OnInit, OnDestroy {
   todos$: Observable<Todos[]>;
+  jokes$: Observable<Jokes[]>;
   displayedColumns: string[] = ['id', 'title', 'status'];
+
+  today;
 
   /** Based on the screen size, switch from standard to one column per row */
   headlines = this.breakpointObserver.observe(Breakpoints.Handset).pipe (
     map(({ matches }) => {
       if (matches) {
         return [
-          { id: 1, title: 'Headline 1', subtitle: 'Subtitle', color: 'deeppink', cols: 4, rows: 1 },
-          { id: 2, title: 'Headline 2', subtitle: 'Subtitle', color: 'deepskyblue', cols: 4, rows: 1 },
-          { id: 3, title: 'Headline 3', subtitle: 'Subtitle', color: 'lawngreen', cols: 4, rows: 1 },
-          { id: 4, title: 'Headline 4', subtitle: 'Subtitle', color: 'orangered', cols: 4, rows: 1 }
+          { id: 1, subtitle: 'Time', color: 'deeppink', cols: 4, rows: 1 },
+          { id: 2, subtitle: 'Date', color: 'deepskyblue', cols: 4, rows: 1 },
+          { id: 3, subtitle: 'Week of year', color: 'lawngreen', cols: 4, rows: 1 },
+          { id: 4, subtitle: 'Year', color: 'orangered', cols: 4, rows: 1 }
         ];
       }
 
       return [
-        { id: 1, title: 'Headline 1', subtitle: 'Subtitle', color: 'deeppink', cols: 1, rows: 1 },
-        { id: 2, title: 'Headline 2', subtitle: 'Subtitle', color: 'deepskyblue', cols: 1, rows: 1 },
-        { id: 3, title: 'Headline 3', subtitle: 'Subtitle', color: 'lawngreen', cols: 1, rows: 1 },
-        { id: 4, title: 'Headline 4', subtitle: 'Subtitle', color: 'orangered', cols: 1, rows: 1 }
+        { id: 1, subtitle: 'Time', color: 'deeppink', cols: 1, rows: 1 },
+        { id: 2, subtitle: 'Date', color: 'deepskyblue', cols: 1, rows: 1 },
+        { id: 3, subtitle: 'Week of year', color: 'lawngreen', cols: 1, rows: 1 },
+        { id: 4, subtitle: 'Year', color: 'orangered', cols: 1, rows: 1 }
       ];
     })
   );
@@ -40,7 +45,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
     map(({ matches }) => {
       if (matches) {
         return [
-          { id: 1, title: 'Card 1', cols: 4, rows: 1 },
+          { id: 1, title: 'Joke of the Day', cols: 4, rows: 1 },
           { id: 2, title: 'Pie Grid', cols: 4, rows: 1 },
           { id: 3, title: 'Todo List', cols: 4, rows: 2 },
           { id: 4, title: 'Bar Vertical', cols: 4, rows: 1 }
@@ -48,7 +53,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
       }
 
       return [
-        { id: 1, title: 'Card 1', cols: 4, rows: 1 },
+        { id: 1, title: 'Joke of the Day', cols: 4, rows: 1 },
         { id: 2, title: 'Pie Grid', cols: 2, rows: 1 },
         { id: 3, title: 'Todo List', cols: 2, rows: 2 },
         { id: 4, title: 'Bar Vertical', cols: 2, rows: 1 }
@@ -56,10 +61,15 @@ export class DashboardComponent implements OnInit, OnDestroy {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver, private apiService: ApiService) {}
+  constructor(private breakpointObserver: BreakpointObserver, private apiService: ApiService) {
+    setInterval(() => {
+      this.today = new Date();
+    }, 1000);
+  }
 
   ngOnInit(): void {
     this.getTodos();
+    this.getJokes();
   }
 
   ngOnDestroy(): void {}
@@ -68,6 +78,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.todos$ = this.apiService.getTodos();
   }
 
-
+  getJokes() {
+    this.jokes$ = this.apiService.getChuckNorrisJokes();
+  }
 
 }
