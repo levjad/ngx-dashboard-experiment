6600bdbda379255b1208d9575e4e0664c4d080bb
levjad <levjad@gmail.com>
2019-01-29 10:06:58 +0100

    Added jokes api

---
+++ src/app/shared/models/jokes.ts
@@ -0,0 +1,6 @@
+export class Jokes {
+    'icon_url': string;
+    'id': string;
+    'url': string;
+    'value': string;
+}
--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Todos } from '../models/todos';
+import { Jokes } from '../models/jokes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   todoApiUrl = 'https://jsonplaceholder.typicode.com/todos';
+  chuckNorrisApiUrl = 'https://api.chucknorris.io/jokes/random';
 
   constructor(private httpClient: HttpClient) { }
 
   public getTodos() {
     return this.httpClient.get<Todos[]>(this.todoApiUrl);
+  }
+
+  public getChuckNorrisJokes() {
+    return this.httpClient.get<Jokes[]>(this.chuckNorrisApiUrl);
   }
 }
